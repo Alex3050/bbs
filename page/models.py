42dad4b2d7baaeb50af612fcfd5e3ecb27a236d2
insomniali <505618233@qq.com>
2021-10-05 09:46:41 +0800

    提交初版发帖

--- page/models.py
+++ page/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class TblPosts(models.Model):
+    last_time = models.IntegerField()
+    tag = models.BinaryField()
+    content = models.BinaryField()
+    comment = models.BinaryField()
+
+    class Meta:
+        managed = False
+        db_table = 'tbl_posts'
